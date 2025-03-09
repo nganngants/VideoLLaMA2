66804f284de0c4a361eaf9a813b42c3d44b7c47b
nganngants <21520069@gm.uit.edu.vn>
2025-03-09 23:16:19 +0700

    add setup script for vast

---
+++ vast_setup_env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+touch ~/.no_auto_tmux
+
+conda env create -f environment.yml
+
+conda init
+
+source ~/.bashrc
+
+conda activate smes_multimodal
+
+apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
+
+pip uninstall bitsandbytes -y
+pip install --upgrade bitsandbytes
+pip install deepspeed
