7d96bbb5979366d5521f10e83c94c9f12b698866
nganngants <21520069@gm.uit.edu.vn>
2025-03-10 00:29:50 +0700

    update

--- vast_setup_env.sh
+++ vast_setup_env.sh
@@ -2,6 +2,8 @@
 
 touch ~/.no_auto_tmux
 
+conda install -c nvidia cuda-compiler
+
 conda env create -f environment.yml
 
 conda init
@@ -15,3 +17,4 @@ apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
 pip uninstall bitsandbytes -y
 pip install --upgrade bitsandbytes
 pip install deepspeed
+pip install transformers
